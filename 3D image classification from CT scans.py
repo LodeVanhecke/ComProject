eb3cbc4db78c0724d45d7b2e2e668300e5abd0c4
Lode Vanhecke <74493130+LodeVanhecke@users.noreply.github.com>
2021-11-11 11:02:44 +0100

    Create 3D image classification from CT scans.py

---
+++ 3D image classification from CT scans.py
@@ -0,0 +1,27 @@
+import os
+import zipfile
+import numpy as np
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# Download url of normal CT scans.
+url = "https://github.com/hasibzunair/3D-image-classification-tutorial/releases/download/v0.2/CT-0.zip"
+filename = os.path.join(os.getcwd(), "CT-0.zip")
+keras.utils.get_file(filename, url)
+
+# Download url of abnormal CT scans.
+url = "https://github.com/hasibzunair/3D-image-classification-tutorial/releases/download/v0.2/CT-23.zip"
+filename = os.path.join(os.getcwd(), "CT-23.zip")
+keras.utils.get_file(filename, url)
+
+# Make a directory to store the data.
+os.makedirs("MosMedData")
+
+# Unzip data in the newly created directory.
+with zipfile.ZipFile("CT-0.zip", "r") as z_fp:
+    z_fp.extractall("./MosMedData/")
+
+with zipfile.ZipFile("CT-23.zip", "r") as z_fp:
+    z_fp.extractall("./MosMedData/")
